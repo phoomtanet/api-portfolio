29e56852b864e29a58bc07d3624a9d60bd13767c
phoomtanet <143348885+phoomtanet@users.noreply.github.com>
2026-02-07 18:11:28 +0700

    update router user add wher condition

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,8 +4,14 @@ import AppError from '../types/app-error';
 
 export const getUsers = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    const users = await listUsers();
-    res.json({ status: 'success', data: users });
+    const limit = Math.max(0, Number.parseInt(_req.query.limit as string, 10) || 10);
+    const offset = Math.max(0, Number.parseInt(_req.query.offset as string, 10) || 0);
+    const userId = _req.query.user_id as string;
+     const keyword = _req.query.keyword as string; 
+
+
+    const usersPage = await listUsers(limit, offset, Number(userId), keyword);
+    res.json({ status: 'success', ...usersPage });
   } catch (error) {
     next(error);
   }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,59 @@
+import { Prisma } from '@prisma/client';
 import prisma from '../config/prisma';
 import AppError from '../types/app-error';
 
-export const listUsers = async () => {
-  const users = await prisma.user.findMany({
-    orderBy: { id: 'asc' },
-  });
-  return users;
+export const listUsers = async (limit: number, offset: number, userId?: number, keyword?: string) => {
+
+  const where: Prisma.userWhereInput = {};
+
+  if (userId) {
+    where.id = userId;
+  }
+  
+  if (keyword) {
+    where.fullname = {
+      contains: keyword,
+    };
+  }
+
+  const [total, users] = await prisma.$transaction([
+    prisma.user.count({ where }),
+    prisma.user.findMany({
+      select: {
+        id: true,
+        fullname: true,
+        username: true,
+        created_at: true,
+        updated_at: true,
+        created_by: true,
+        updated_by: true,
+      },
+      where,
+      orderBy: { id: 'asc' },
+      take: limit,
+      skip: offset,
+    }),
+  ]);
+
+  return { data: users, total, limit, offset };
 };
 
 export const getUserById = async (id: number) => {
-  const user = await prisma.user.findUnique({ where: { id } });
+  const user = await prisma.user.findUnique({
+    select: {
+      id: true,
+      fullname: true,
+      username: true,
+      created_at: true,
+      updated_at: true,
+      created_by: true,
+      updated_by: true,
+    },
+    where: { id },
+  });
   if (!user) {
     throw new AppError('User not found', 404);
   }
+
   return user;
 };
